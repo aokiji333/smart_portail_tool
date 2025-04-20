665ebea127f2f8b8d62b7d7b3256030bb4e6a907
aokiji333 <stevalight@gmail.com>
2025-04-20 04:19:28 +0200

    Update main.c

--- applications_user/smart_portail_tool/main.c
+++ applications_user/smart_portail_tool/main.c
@@ -1,7 +1,7 @@
 #include <furi.h>
 #include <furi_hal.h>
 
-int32_t smart_portail_tool_app(void* p) {
+int32_t smart_portail_tool(void* p) {
     UNUSED(p);
     furi_hal_gpio_write(&gpio_led_red, true);
     furi_delay_ms(1000);
